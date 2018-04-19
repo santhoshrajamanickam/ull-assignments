a0f80e997c1d3b170142279bb57fb13212e1936e
Daniel <dfdazac@gmail.com>
2018-04-19 21:23:34 +0200

    Finalizing practical 1

--- word-representations/README.md
+++ word-representations/README.md
@@ -1,3 +1,5 @@
+All our results are shown in the notebook [evaluating-word-representations.ipynb](evaluating-word-representations.ipynb)  
+
 The following files are required in the data folder:
 
 **Word embeddings** [link](https://levyomer.wordpress.com/2014/04/25/dependency-based-word-embeddings/)  
@@ -9,5 +11,5 @@ The following files are required in the data folder:
 `MEN_dataset_natural_form_full` [link](https://www.cl.cam.ac.uk/~fh295/simlex.html)  
 `SimLex-999.txt` [link](https://staff.fnwi.uva.nl/e.bruni/MEN)
 
-**Clustering words**
-`Given 2000 frequent nouns` [link](https://uva-slpl.github.io/ull/resources/practicals/practical1/2000_nouns_sorted.txt) 
+**Noun lists**  
+`2000_nouns_sorted.txt` [link](https://uva-slpl.github.io/ull/resources/practicals/practical1/2000_nouns_sorted.txt) 
--- word-representations/clustering.py
+++ word-representations/clustering.py
@@ -1,10 +1,6 @@
 import numpy as np
 from collections import defaultdict
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-from ggplot import *
-import pandas as pd
 
 def cluster_words(embedding_dict, frequent_nouns, num_clusters):
     """
@@ -37,7 +33,6 @@ def cluster_words(embedding_dict, frequent_nouns, num_clusters):
 
     return embeddings, labels['BOW2'], labels['BOW5'], labels['Dependency']
 
-
 def print_cluster_words(cluster, words, labels):
     """
     Prints the 10 or less words with cluster as its label
@@ -60,39 +55,3 @@ def print_cluster_words(cluster, words, labels):
             num_words = num_words + 1
 
     print(cluster_words)
-
-
-def visualize_pca(embeddings, labels, emb_type):
-    """
-    Uses PCA to reduce the dimension of the features and then visualize using the
-    labels to assign different colors to different cluster words. Uses random 200
-    words for brevity and plots a 2D plot of the words.
-    Args:
-        - embeddings (list): embedding list containing the features for each of the words in the vocabulary
-        - labels (list): list containing the labels of the words in the vocabulary
-        - emb_type (str): specifies the type of word embedding representation
-    """
-
-    feat_cols = [ 'feature'+str(i) for i in range(len(embeddings[1])) ]
-
-    df = pd.DataFrame(embeddings,columns=feat_cols)
-    df['label'] = labels
-    df['label'] = df['label'].apply(lambda i: str(i))
-
-    rndperm = np.random.permutation(df.shape[0])
-
-    pca = PCA(n_components=3)
-    pca_result = pca.fit_transform(df[feat_cols].values)
-
-    df['pca-one'] = pca_result[:,0]
-    df['pca-two'] = pca_result[:,1]
-    df['pca-three'] = pca_result[:,2]
-
-    # print("Explained variation per principal component: {}".format(pca.explained_variance_ratio_))
-
-    graph_title = emb_type + ": 1st and 2nd principal components colored by cluster belonging words"
-
-    chart = ggplot( df.loc[rndperm[:200],:], aes(x='pca-one', y='pca-two', color='label') ) \
-        + geom_point(size=75,alpha=0.8) \
-        + ggtitle(graph_title)
-    print(chart)
