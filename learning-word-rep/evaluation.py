6f27d3a879ac5dba164cdb5f7d78a4f975b085bf
Santhosh Kumar Rajamanickam <santhosh.rajamanickam@student.uva.nl>
2018-05-15 17:53:48 +0200

    Update evaluation.py

--- learning-word-rep/evaluation.py
+++ learning-word-rep/evaluation.py
@@ -3,17 +3,21 @@ import string
 import torch
 from skipgram import Skipgram
 import pickle
+import re
 
 class Eval:
 
-    def __init__(self, model):
-        self.model=model
-        self.test_sentences = defaultdict(lambda : defaultdict(lambda : set()))
+    def __init__(self, model, window_size=None):
+        self.model = model
+        self.window_size = window_size
+        self.test_sentences = defaultdict(lambda : defaultdict(lambda : []))
+        self.candidates = defaultdict(lambda : set())
 
-    def load_test_sentences(self, test_sentences_path):
+    def load_test_sentences(self, test_sentences_path, candidates_path):
 
         with open(test_sentences_path) as file:
             for line in file:
+                num_padding = 0
                 # Split into tokens
                 sentence = line.strip().split()
                 target_word = sentence.pop(0)
@@ -21,18 +25,38 @@ class Eval:
                 target_position = sentence.pop(0)
                 # Remove the target word
                 sentence.pop(int(target_position))
+                # Extract context words
+                start = int(target_position) - self.window_size
+                end = int(target_position) + 1 + self.window_size
+                if start < 0:
+                    num_padding += abs(start)
+                    start = 0
+                if end > len(sentence):
+                    num_padding += end - len(sentence)
+                    end = len(sentence)
+                context_words = sentence[start:int(target_position)] + sentence[int(target_position):end]
                 # Remove punctuation
-                # context_words = sentence.translate(str.maketrans('', '', string.punctuation))
-                context_words = [s.translate(str.maketrans('', '', string.punctuation)) for s in sentence]
-                context_words = set(s for s in context_words if s)
-                # print(target_word, id_sentence, context_words)
+                context_words = [s.translate(str.maketrans('', '', string.punctuation)) for s in context_words]
+                context_words = [s for s in context_words if s]
+                context_words += ['<s>' for padding_count in range(0,num_padding)]
+                # print(context_words)
                 self.test_sentences[target_word][id_sentence] = context_words
-                # break
+
+        with open(candidates_path) as file:
+            for line in file:
+                # print(line)
+                candidates =re.split('\W+',line)
+                target_word = candidates.pop(0)
+                candidates.pop(0)
+                candidates = [s for s in candidates if s]
+                self.candidates[target_word] = candidates
+
 
     def score_context_words(self):
 
         missing_count = 0
         target_count = 0
+        cos = torch.nn.CosineSimilarity(dim=0)
 
         if self.model == 'skipgram':
             # Initialize model (add 2 words to vocab_size for UNK and EOS)
@@ -54,30 +78,56 @@ class Eval:
                 words = target_word.split(".")
                 target = words[0]
                 if target in word2idx:
-                    in_emb = skipgram.embeddings(torch.tensor(word2idx[target], dtype=torch.long))
+                    target_emb = skipgram.embeddings(torch.tensor(word2idx[target], dtype=torch.long))
                 else:
                     print('missing target: ' + str(target_word))
                     missing_count += 1
                     continue
 
+                candidate_embs =defaultdict()
+                for candidate in self.candidates[target]:
+                    if candidate in word2idx:
+                        candidate_embs[candidate] = skipgram.embeddings(torch.tensor(word2idx[candidate], dtype=torch.long))
+                    else:
+                        candidate_embs[candidate] = torch.zeros(target_emb.shape)
+
                 for sentence_id in self.test_sentences[target_word].keys():
                     file.write('RANKED|\t')
                     file.write(str(target_word) + '\t')
-                    file.write(str(sentence_id))
+                    file.write(str(sentence_id)+'|')
 
-                    for context_word in self.test_sentences[target_word][sentence_id]:
-                        if context_word in word2idx:
-                            out_emb = skipgram.out_embeddings(torch.tensor(word2idx[context_word], dtype=torch.long))
-                            score = torch.dot(in_emb, out_emb).item()
-                            file.write('|\t' + str(context_word)+'\t'+str(score))
-                            # print(score)
-                        else:
-                            # print(context_word)
-                            # file.write(str(context_word) + ' ' + str(0) + '|\t')
-                            out_emb = skipgram.out_embeddings(torch.tensor(word2idx['<unk>'], dtype=torch.long))
-                            score = torch.dot(in_emb, out_emb).item()
-                            file.write('|\t' + str(context_word) + '\t' + str(score))
+                    for candidate in self.candidates[target]:
+                        score = 0
+                        score += cos(target_emb,candidate_embs[candidate])
+                        num_context = 0
+                        for context_word in self.test_sentences[target_word][sentence_id]:
+                            if context_word in word2idx:
+                                context_emb = skipgram.out_embeddings(
+                                    torch.tensor(word2idx[context_word], dtype=torch.long))
+                                score += cos(candidate_embs[candidate],context_emb)
+                                num_context += 1
+
+                        score /= num_context + 1
+                        score = score.data.item()
+                        file.write('\t' + str(candidate) + '\t' + str(score)+'|')
                     file.write('\n')
+
+
+
+
+                    # for context_word in self.test_sentences[target_word][sentence_id]:
+                    #     if context_word in word2idx:
+                    #         out_emb = skipgram.out_embeddings(torch.tensor(word2idx[context_word], dtype=torch.long))
+                    #         score = torch.dot(in_emb, out_emb).item()
+                    #         file.write('\t' + str(context_word)+'\t'+str(score))
+                    #         # print(score)
+                    #     else:
+                    #         # print(context_word)
+                    #         # file.write(str(context_word) + ' ' + str(0) + '|\t')
+                    #         out_emb = skipgram.out_embeddings(torch.tensor(word2idx['<unk>'], dtype=torch.long))
+                    #         score = torch.dot(in_emb, out_emb).item()
+                    #         file.write('\t' + str(context_word) + '\t' + str(score))
+                    # file.write('\n')
 
             file.close()
             print('Target count: '+str(target_count))
--- learning-word-rep/test_evaluation.py
+++ learning-word-rep/test_evaluation.py
@@ -1,7 +1,8 @@
 from evaluation import Eval
 
 test_sentences_path = './lst/lst_test.preprocessed'
+candidates_path = './lst/lst.gold.candidates'
 
-eval = Eval(model='skipgram')
-eval.load_test_sentences(test_sentences_path=test_sentences_path)
+eval = Eval(model='skipgram',window_size=5)
+eval.load_test_sentences(test_sentences_path=test_sentences_path, candidates_path=candidates_path)
 eval.score_context_words()
