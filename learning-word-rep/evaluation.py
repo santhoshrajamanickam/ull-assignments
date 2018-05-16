06180eb7cde66010b45653cdc66ed8775e7607e7
Daniel <dfdazac@gmail.com>
2018-05-16 20:16:41 +0200

    Added evaluation for bayesian and embed-align

--- learning-word-rep/evaluation.py
+++ learning-word-rep/evaluation.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 import string
 import torch
+import torch.nn.functional as F
 from skipgram import Skipgram
 from bayes_skipgram import BayesianSkipgram
+from embed_align import EmbedAlign
 import pickle
 import re
 
@@ -11,6 +13,8 @@ class Eval:
         self.model = model
         self.window_size = window_size
         self.test_sentences = defaultdict(lambda : defaultdict(lambda : []))
+        self.sentences = defaultdict(lambda: defaultdict(lambda: []))
+        self.target_pos = defaultdict(lambda: defaultdict(lambda: []))
         self.candidates = defaultdict(lambda : set())
 
     def load_test_sentences(self, test_sentences_path, candidates_path):
@@ -58,6 +62,8 @@ class Eval:
                 context_words += ['<s>'] * end_padding
 
                 self.test_sentences[target_word][id_sentence] = context_words
+                self.sentences[target_word][id_sentence] = sentence
+                self.target_pos[target_word][id_sentence] = target_position
 
         # Read candidates per word
         with open(candidates_path) as file:
@@ -103,10 +109,7 @@ class Eval:
 
                 candidate_embs =defaultdict()
                 for candidate in self.candidates[target]:
-                    if candidate in word2idx:
-                        candidate_embs[candidate] = skipgram.embeddings(torch.tensor(word2idx[candidate], dtype=torch.long))
-                    else:
-                        candidate_embs[candidate] = torch.zeros(target_emb.shape)
+                    candidate_embs[candidate] = skipgram.embeddings(torch.tensor(word2idx.get(candidate, word2idx['<unk>']), dtype=torch.long))
 
                 for sentence_id in self.test_sentences[target_word].keys():
                     file.write('RANKED\t')
@@ -143,6 +146,100 @@ class Eval:
             file = open('./lst_bayesian.out', 'w')
 
             for target_word in self.test_sentences.keys():
+                words = target_word.split(".")
+                target = words[0]
+
+                for sentence_id in self.test_sentences[target_word].keys():
+                    file.write('RANKED\t')
+                    file.write(str(target_word)+' ')
+                    file.write(str(sentence_id))
+
+                    context = self.test_sentences[target_word][sentence_id]
+                    # Get target and context indices
+                    target_idx_t = torch.tensor([word2idx.get(target, word2idx['<unk>'])], dtype=torch.long)
+                    context_idx_t = torch.tensor([[word2idx.get(word, word2idx['<unk>']) for word in context]], dtype=torch.long)
+
+                    mu, sigma = bayes_forward(bayesian, target_idx_t, context_idx_t)
+
+                    for candidate in self.candidates[target]:
+                        candidate_idx = torch.tensor([word2idx.get(candidate, word2idx['<unk>'])], dtype=torch.long)
+                        mu_x = bayesian.L(candidate_idx)
+                        sigma_x = F.softplus(bayesian.S(candidate_idx))
+                        score = torch.log(sigma_x) - torch.log(sigma) + 0.5 * (sigma**2 + (mu - mu_x)**2)/sigma_x**2 - 0.5
+                        score = score.sum().data.item()
+                        file.write('\t' + str(candidate) + ' ' + str(score))
+                    file.write('\n')
+
+        elif self.model == 'embedalign':
+            # Initialize model (add 2 words to vocab_size for UNK and EOS)
+            embedalign = EmbedAlign(10002, 10002, emb_dimensions=300)
+            # Load from training results
+            embedalign.load_state_dict(torch.load('10000V_300d_EmbedAlign.pt', map_location='cpu'))
+
+            # print(self.test_sentences.keys())
+            file = open('./lst_embedalign.out', 'w')
+
+            for target_word in self.sentences.keys():
+                words = target_word.split(".")
+                target = words[0]
+
+                for sentence_id in self.sentences[target_word].keys():
+                    file.write('RANKED\t')
+                    file.write(str(target_word)+' ')
+                    file.write(str(sentence_id))
+
+                    sentence = self.sentences[target_word][sentence_id]
+                    # Get target and context indices
+                    sentence_idx_t = torch.tensor([[word2idx.get(word, word2idx['<unk>']) for word in sentence]], dtype=torch.long)
+
+                    mu, sigma = embedalign_forward(embedalign, sentence_idx_t)
+
+                    for candidate in self.candidates[target]:
+                        candidate_sentence = sentence
+                        candidate_sentence[self.target_pos[target_word][sentence_id]] = candidate
+
+                        cand_sentence_idx_t = torch.tensor([[word2idx.get(word, word2idx['<unk>']) for word in candidate_sentence]], dtype=torch.long)
+                        mu_x, sigma_x = embedalign_forward(embedalign, cand_sentence_idx_t)
+
+                        score = torch.log(sigma_x) - torch.log(sigma) + 0.5 * (sigma**2 + (mu - mu_x)**2)/sigma_x**2 - 0.5
+                        score = score.sum().data.item()
+                        file.write('\t' + str(candidate) + ' ' + str(score))
+                    file.write('\n')
 
 
 
+def bayes_forward(model, word, context):
+    word_emb = model.embeddings(word)
+    context_embs = model.embeddings(context)
+    n_batch, n_context, n_dim = context_embs.shape
+    # Concatenate word and context embeddings
+    concat_emb = torch.cat((word_emb.expand(n_context, -1, -1).t(), context_embs), dim=-1)
+    concat_sum = torch.sum(F.relu(concat_emb), dim=1)
+    # Calculate inference parameters
+    mu = model.affine_mu(concat_sum)
+    sigma = F.softplus(model.affine_sigma(concat_sum))
+
+    return mu, sigma
+
+def embedalign_forward(model, sentence):
+    """ Calculates the ELBO for the Embed-align model.
+    Args:
+        - sentence1 (tensor): (N, m), a tensor containing sentences in L1
+        - sentence2 (tensor): (N, n), a tensor containing sentences in L2
+    Returns:
+        - tensor: (1), the loss.
+    """
+    # Get embeddings for words in sentence
+    sent_embeddings = model.embeddings(sentence)
+    n_batch, m1, n_dim = sent_embeddings.shape
+
+    # - Encoder -
+    # Get output from bidirectional LSTM
+    out, _ = model.bilstm(sent_embeddings.t())
+    # Sum hidden states from both directions
+    out = (out[:, :, :model.emb_dimensions] + out[:, :, :model.emb_dimensions]).t()
+    # Calculate inference parameters
+    mu = model.affine2_mu(F.relu(model.affine1_mu(out)))
+    sigma = F.softplus(model.affine2_sigma(F.relu(model.affine1_sigma(out))))
+
+    return mu, sigma
--- learning-word-rep/test_evaluation.py
+++ learning-word-rep/test_evaluation.py
@@ -3,6 +3,8 @@ from evaluation import Eval
 test_sentences_path = './lst/lst_test.preprocessed'
 candidates_path = './lst/lst.gold.candidates'
 
-eval = Eval(model='skipgram',window_size=5)
+#eval = Eval(model='skipgram',window_size=5)
+#eval = Eval(model='bayesian',window_size=5)
+eval = Eval(model='embedalign',window_size=0)
 eval.load_test_sentences(test_sentences_path=test_sentences_path, candidates_path=candidates_path)
 eval.score_context_words()
